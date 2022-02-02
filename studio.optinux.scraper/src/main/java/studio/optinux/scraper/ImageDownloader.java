b4a521c1c45dd7b3a200e0b84c4fddfe8f2b3a5a
Optinux <tobias.eglseder2@gmail.com>
2022-02-02 20:09:12 +0100

    comment code

--- studio.optinux.scraper/src/main/java/studio/optinux/scraper/ImageDownloader.java
+++ studio.optinux.scraper/src/main/java/studio/optinux/scraper/ImageDownloader.java
@@ -11,30 +11,37 @@ public class ImageDownloader {
 
   public static void DownloadImage(String search, String path)
     throws IOException {
-    InputStream inputStream = null; //start IO-stuff
-    OutputStream outputStream = null; //start IO-stuff
+    InputStream inputStream = null; // start IO-stuff
+    OutputStream outputStream = null; // start IO-stuff
     try {
-      URL url = new URL(search); //set URL
+      URL url = new URL(search); // set URL
       String USER_AGENT =
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"; //proper User-Agent -> no getting blocked
-      URLConnection con = url.openConnection(); //start the connection
-      con.setRequestProperty("User-Agent", USER_AGENT); //set User-Agent
-      inputStream = con.getInputStream(); //get Data from server
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"; // proper
+      // User-Agent
+      // ->
+      // no
+      // getting
+      // blocked
+      URLConnection con = url.openConnection(); // start the connection
+      con.setRequestProperty("User-Agent", USER_AGENT); // set User-Agent
+      inputStream = con.getInputStream(); // get Data from server
       outputStream = new FileOutputStream(path);
-      byte[] buffer = new byte[2048]; //limit to one file per loop, someone on Discord told me to use this lol
-      int length; //limit to one file per loop, someone on Discord told me to use this lol
+      byte[] buffer = new byte[2048]; // limit to one file per loop, someone on Discord told me to use this lol
+      int length; // limit to one file per loop, someone on Discord told me to use this lol
       while ((length = inputStream.read(buffer)) != -1) {
-        outputStream.write(buffer, 0, length); //write to Disk
+        outputStream.write(buffer, 0, length); // write to Disk
       }
     } catch (Exception ex) {
       System.out.println("Something broke!");
     }
-    outputStream.close(); //close the IO stuff
-    inputStream.close(); //close the IO stuff
+    outputStream.close(); // close the IO stuff
+    inputStream.close(); // close the IO stuff
   }
 
   public static void main(String[] args) throws IOException {
     ImageDownloader id1 = new ImageDownloader();
-    //  id1.DownloadImage("https://www.wikimedia.de/wp-content/uploads/2020/12/Wikipedia_mini_globe_handheld-720x405.jpg", "./output/test.jpg");   <- Example for testing
+    /*
+     * id1.DownloadImage("https://www.wikimedia.de/wp-content/uploads/2020/12/Wikipedia_mini_globe_handheld-720x405.jpg",./output/test.jpg");
+     */
   }
-}
+} // RedHat Java Formatter doing funky stuff already huh, the code aint even that big
--- studio.optinux.scraper/src/main/java/studio/optinux/scraper/ParseExcel.java
+++ studio.optinux.scraper/src/main/java/studio/optinux/scraper/ParseExcel.java
@@ -12,56 +12,50 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ParseExcel extends ImageDownloader {
 
-  public static String CurrentStringURL; //create variables to pass onto the Downloader
-  public static String CurrentStringPATH; //create variables to pass onto the Downloader
-  public static CellAddress CellAddress; //create variables to pass onto the Downloader
+  public static String CurrentStringURL; // create variables to pass onto the Downloader
+  public static String CurrentStringPATH; // create variables to pass onto the Downloader
+  public static CellAddress CellAddress; // create variables to pass onto the Downloader
 
   public static void main(String[] args) throws IOException {
-    String excelFilePath = "catalog-edit.xlsx";
-    FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+    String excelFilePath = "catalog-edit.xlsx"; // specify the file that is being used
+    FileInputStream inputStream = new FileInputStream(new File(excelFilePath)); // start IO-Stuff
 
-    Workbook workbook = new XSSFWorkbook(inputStream);
+    Workbook workbook = new XSSFWorkbook(inputStream); // basically read the .xlsx
     org.apache.poi.ss.usermodel.Sheet firstSheet = workbook.getSheetAt(0);
     Iterator<Row> iterator = firstSheet.iterator();
 
-    while (iterator.hasNext()) {
+    while (iterator.hasNext()) { // stop after there is no row left
       Row nextRow = iterator.next();
       Iterator<Cell> cellIterator = nextRow.cellIterator();
 
-      while (cellIterator.hasNext()) {
-        Cell cell = cellIterator.next();
+      while (cellIterator.hasNext()) { // stop after there is no cell left
+        Cell cell = cellIterator.next(); // iterate through excel sheet cells
 
-        CellAddress = cell.getAddress();
-        System.out.println(CellAddress);
+        CellAddress = cell.getAddress(); // get cellAdress
+        System.out.println(CellAddress); // DEBUG
 
-        String temp = CellAddress; //TODO: get this to work
+        String temp = CellAddress; // TODO: get this to work
 
-        char firstChar = temp.charAt(0); //take first Character from the newly created String
+        char firstChar = temp.charAt(0); // take first Character from the newly created String
 
-        if (firstChar == 'A') { //check if its from Column A -> URL
+        if (firstChar == 'A') { // check if its from Column A -> URL
           CurrentStringURL = cell.getStringCellValue();
           System.out.println(CurrentStringURL);
-          break;
         }
 
-        if (firstChar == 'B') { //check if its from Column B  -> PATH
+        if (firstChar == 'B') { // check if its from Column B -> PATH
           CurrentStringPATH = cell.getStringCellValue();
           System.out.println(CurrentStringPATH);
-          break;
         }
       }
-      System.out.print(" -> ");
+      CurrentStringURL = CurrentStringURL.replace("html", "jpg"); // Replace .html w/ .jpg -> ImageDownloader
+      CurrentStringPATH = "./output/" + CurrentStringPATH + ".jpg"; // Convert "number" into usable file path
+      System.out.println("Updated URL Input: " + CurrentStringURL); // DEBUG
+      System.out.println("Updated PATH Input: " + CurrentStringPATH); // DEBUG
+      DownloadImage(CurrentStringURL, CurrentStringPATH); // execute ImageDownloader
     }
-
-    CurrentStringURL = CurrentStringURL.replace("html", "jpg"); //Replace .html w/ .jpg -> ImageDownloader
-    CurrentStringPATH = "./output/" + CurrentStringPATH + ".jpg"; //Convert "number" into usable file path
-    System.out.println("Updated URL Input: " + CurrentStringURL);
-    System.out.println("Updated PATH Input: " + CurrentStringPATH);
-    DownloadImage(CurrentStringURL, CurrentStringPATH); //execute ImageDownloader
-
     workbook.close();
     inputStream.close();
   }
 }
-//TODO: comment code so it looks better + formatting
-//this took way longer than it should have -Optinux
+// this took way longer than it should have -Optinux
