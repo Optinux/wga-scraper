93096e5adc5b2a773d8d3473770f82a9a2161566
Optinux <tobias.eglseder2@gmail.com>
2022-02-02 19:33:38 +0100

    code cleanup

--- studio.optinux.scraper/src/main/java/studio/optinux/scraper/ParseExcel.java
+++ studio.optinux.scraper/src/main/java/studio/optinux/scraper/ParseExcel.java
@@ -4,64 +4,64 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
-
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ParseExcel extends ImageDownloader{
+public class ParseExcel extends ImageDownloader {
 
-public static String CurrentStringURL;   //create variables to pass onto the Downloader
-public static String CurrentStringPATH;   //create variables to pass onto the Downloader
-public static CellAddress CellAddress;       //create variables to pass onto the Downloader
+  public static String CurrentStringURL; //create variables to pass onto the Downloader
+  public static String CurrentStringPATH; //create variables to pass onto the Downloader
+  public static CellAddress CellAddress; //create variables to pass onto the Downloader
 
   public static void main(String[] args) throws IOException {
     String excelFilePath = "catalog-edit.xlsx";
     FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
-     
+
     Workbook workbook = new XSSFWorkbook(inputStream);
     org.apache.poi.ss.usermodel.Sheet firstSheet = workbook.getSheetAt(0);
     Iterator<Row> iterator = firstSheet.iterator();
-     
-    while (iterator.hasNext()) {
-        Row nextRow = iterator.next();
-        Iterator<Cell> cellIterator = nextRow.cellIterator();
-         
-        while (cellIterator.hasNext()) {
-            Cell cell = cellIterator.next();
-             
-              CellAddress = cell.getAddress();
-              System.out.println(CellAddress);
-                      
-              if(CellAddress == A){                                   //TODO: get this to work
-                CurrentStringURL = cell.getStringCellValue();
-                System.out.println(CurrentStringURL);
-                break;
-              }
-             if(CellAddress == B){                                        //TODO: get this to work
-              CurrentStringPATH = cell.getStringCellValue();
-              System.out.println(CurrentStringPATH);
-              break;
-             }
 
-                  }
-            System.out.print(" -> ");
+    while (iterator.hasNext()) {
+      Row nextRow = iterator.next();
+      Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+      while (cellIterator.hasNext()) {
+        Cell cell = cellIterator.next();
+
+        CellAddress = cell.getAddress();
+        System.out.println(CellAddress);
+
+        String temp = CellAddress; //TODO: get this to work
+
+        char firstChar = temp.charAt(0); //take first Character from the newly created String
+
+        if (firstChar == 'A') { //check if its from Column A -> URL
+          CurrentStringURL = cell.getStringCellValue();
+          System.out.println(CurrentStringURL);
+          break;
         }
 
-        CurrentStringURL = CurrentStringURL.replace("html", "jpg");      //Replace .html w/ .jpg -> ImageDownloader
-        CurrentStringPATH = "./output/" + CurrentStringPATH + ".jpg";    //Convert "number" into usable file path
-        System.out.println("Updated URL Input: " + CurrentStringURL);       
-        System.out.println("Updated PATH Input: " + CurrentStringPATH);
-        DownloadImage(CurrentStringURL, CurrentStringPATH);     //execute ImageDownloader
+        if (firstChar == 'B') { //check if its from Column B  -> PATH
+          CurrentStringPATH = cell.getStringCellValue();
+          System.out.println(CurrentStringPATH);
+          break;
+        }
+      }
+      System.out.print(" -> ");
     }
-     
-  //  workbook.close();
-   // inputStream.close();
+
+    CurrentStringURL = CurrentStringURL.replace("html", "jpg"); //Replace .html w/ .jpg -> ImageDownloader
+    CurrentStringPATH = "./output/" + CurrentStringPATH + ".jpg"; //Convert "number" into usable file path
+    System.out.println("Updated URL Input: " + CurrentStringURL);
+    System.out.println("Updated PATH Input: " + CurrentStringPATH);
+    DownloadImage(CurrentStringURL, CurrentStringPATH); //execute ImageDownloader
+
+    workbook.close();
+    inputStream.close();
+  }
 }
-	
-
-
-//TODO: comment code so it looks better
+//TODO: comment code so it looks better + formatting
 //this took way longer than it should have -Optinux
